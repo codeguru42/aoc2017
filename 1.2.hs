71e45df6bcb3916374dcd4a88434e7b8456d4e50
Code Apprentice <codeguru42@gmail.com>
2017-11-30 22:47:02 -0700

    Day 1 Part 2: Final solution

---
+++ 1.2.hs
@@ -0,0 +1,13 @@
+import Data.Char (digitToInt)
+import Data.Maybe (catMaybes)
+
+pairs xs= zip xs (drop n xs ++ take n xs)
+    where n = length xs `div` 2
+
+matches :: [(Int, Int)] -> [Maybe Int]
+matches = map (\(x, y) -> if x == y then Just x else Nothing)
+
+main = do
+    input <- readFile "1.txt"
+    let digits = map digitToInt input
+    print . sum . catMaybes . matches $ pairs digits
