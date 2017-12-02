febf49e72de70d2faf71336fa48bb62c09fb6579
Code Apprentice <codeguru42@gmail.com>
2017-12-01 21:26:26 -0700

    Day 1: Refactor parsing input to solution functions

--- 1.hs
+++ 1.hs
@@ -2,10 +2,10 @@ import Data.Char (digitToInt)
 import Data.Maybe (catMaybes)
 import Test.HUnit
 
-part1Fixtures = [ ("test1_1", 3, [1, 1, 2, 2])
-                , ("test1_2", 4, [1, 1, 1, 1])
-                , ("test1_3", 0, [1, 2, 3, 4])
-                , ("test1_1", 9, [9, 1, 2, 1, 2, 9])
+part1Fixtures = [ ("test1_1", 3, "1122")
+                , ("test1_2", 4, "1111")
+                , ("test1_3", 0, "1234")
+                , ("test1_1", 9, "912129")
                 ]
 part1Test (label, expected, input) = label ~: expected ~=? (part1 input)
 part1Tests = test $ map part1Test part1Fixtures
@@ -23,15 +23,16 @@ matches = map (\(x, y) -> if x == y then Just x else Nothing)
 day1 ::  [(Int, Int)] -> Int
 day1 = sum . catMaybes . matches
 
-part1 :: [Int] -> Int
-part1 = day1 . pairs1
+part1 :: String -> Int
+part1 = day1 . pairs1 . parse
 
-part2 :: [Int] -> Int
-part2 = day1 . pairs2
+part2 :: String -> Int
+part2 = day1 . pairs2 . parse
+
+parse = map digitToInt
 
 main = do
     runTestTT part1Tests
     input <- readFile "1.txt"
-    let digits = map digitToInt input
-    print $ part1 digits
-    print $ part2 digits
+    print $ part1 input
+    print $ part2 input
