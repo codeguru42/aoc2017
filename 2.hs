f2534d754a4aad6ed6a3eb1c2ba19e76699f4df9
Code Apprentice <codeguru42@gmail.com>
2017-12-01 23:17:11 -0700

    Day 2: Modify checksum function

--- 2.hs
+++ 2.hs
@@ -10,7 +10,7 @@ testSplitAll = "testSplitAll" ~: [ [pack "foo", pack "bar"]
 testParse = "testParse" ~: [[1, 2], [3, 4]] ~=? parse "1\t2\n3\t4\n"
 testDiff = "testDiff" ~: 6 ~=? diff [2, 7, 8, 3, 6, 4]
 testDiffAll = "testDiffAll" ~: [5, 3] ~=? diffAll [[2, 7, 5], [3, 6, 4]]
-testChecksum = "testChecksum" ~: 8 ~=? checksum [[2, 7, 5], [3, 6, 4]]
+testChecksum = "testChecksum" ~: 8 ~=? checksum "2\t7\t 5\n3\t6\t4\n"
 
 splitOnTabs = T.splitOn (pack "\t")
 splitAll = (Prelude.map splitOnTabs) . T.lines . pack
@@ -18,8 +18,7 @@ parse = Prelude.map (Prelude.map $ (read :: String -> Int) . unpack) . splitAll
 
 diff xs = Prelude.maximum xs - Prelude.minimum xs
 diffAll = Prelude.map diff
-
-checksum = sum . diffAll
+checksum = sum . diffAll . parse
 
 main = do
     runTestTT allTests
