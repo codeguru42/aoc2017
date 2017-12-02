cd0a083bde36dfffed6611d0a5f590820f2966ea
Code Apprentice <codeguru42@gmail.com>
2017-12-01 23:12:09 -0700

    Day 2: One more helper function

--- 2.hs
+++ 2.hs
@@ -1,7 +1,7 @@
 import Test.HUnit
 import Data.Text as T
 
-allTests = test [testSplitOnTabs, testSplitAll, testParse, testDiff]
+allTests = test [testSplitOnTabs, testSplitAll, testParse, testDiff, testDiffAll]
 testSplitOnTabs = "testSplitOnTabs" ~: [pack "foo", pack "bar"] ~=? (splitOnTabs $ pack "foo\tbar")
 testSplitAll = "testSplitAll" ~: [ [pack "foo", pack "bar"]
                                    , [pack "fooze", pack "baz"]
@@ -9,12 +9,14 @@ testSplitAll = "testSplitAll" ~: [ [pack "foo", pack "bar"]
                               ~=? splitAll "foo\tbar\nfooze\tbaz\n"
 testParse = "testParse" ~: [[1, 2], [3, 4]] ~=? parse "1\t2\n3\t4\n"
 testDiff = "testDiff" ~: 6 ~=? diff [2, 7, 8, 3, 6, 4]
+testDiffAll = "testDiffAll" ~: [5, 3] ~=? diffAll [[2, 7, 5], [3, 6, 4]]
 
 splitOnTabs = T.splitOn (pack "\t")
 splitAll = (Prelude.map splitOnTabs) . T.lines . pack
 parse = Prelude.map (Prelude.map $ (read :: String -> Int) . unpack) . splitAll
 
 diff xs = Prelude.maximum xs - Prelude.minimum xs
+diffAll = Prelude.map diff
 
 main = do
     runTestTT allTests
