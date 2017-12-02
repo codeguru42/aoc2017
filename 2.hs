cbadfe78d1c076413500bccf84a9da6f74f23c7a
Code Apprentice <codeguru42@gmail.com>
2017-12-01 22:47:50 -0700

    Day 2: Rename second helper function

--- 2.hs
+++ 2.hs
@@ -1,16 +1,16 @@
 import Test.HUnit
 import Data.Text as T
 
-allTests = test [testSplitOnTabs, testParse]
+allTests = test [testSplitOnTabs, testSplitAll]
 testSplitOnTabs = "testSplitOnTabs" ~: [pack "foo", pack "bar"] ~=? (splitOnTabs $ pack "foo\tbar")
-testParse = "testParse" ~: [ [pack "foo", pack "bar"]
-                           , [pack "fooze", pack "baz"]
-                           ]
-                        ~=? parse "foo\tbar\nfooze\tbaz\n"
+testSplitAll = "testSplitAll" ~: [ [pack "foo", pack "bar"]
+                                   , [pack "fooze", pack "baz"]
+                                 ]
+                              ~=? splitAll "foo\tbar\nfooze\tbaz\n"
 
 splitOnTabs = T.splitOn (pack "\t")
 
-parse = (Prelude.map splitOnTabs) . T.lines . pack
+splitAll = (Prelude.map splitOnTabs) . T.lines . pack
 
 main = do
     runTestTT allTests
