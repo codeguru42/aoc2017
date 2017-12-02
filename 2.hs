20251982e9c9d504d208c7d33fa43909248e0f86
Code Apprentice <codeguru42@gmail.com>
2017-12-01 23:15:56 -0700

    Day 2: Calculate checksum

--- 2.hs
+++ 2.hs
@@ -1,7 +1,7 @@
 import Test.HUnit
 import Data.Text as T
 
-allTests = test [testSplitOnTabs, testSplitAll, testParse, testDiff, testDiffAll]
+allTests = test [testSplitOnTabs, testSplitAll, testParse, testDiff, testDiffAll, testChecksum]
 testSplitOnTabs = "testSplitOnTabs" ~: [pack "foo", pack "bar"] ~=? (splitOnTabs $ pack "foo\tbar")
 testSplitAll = "testSplitAll" ~: [ [pack "foo", pack "bar"]
                                    , [pack "fooze", pack "baz"]
@@ -10,6 +10,7 @@ testSplitAll = "testSplitAll" ~: [ [pack "foo", pack "bar"]
 testParse = "testParse" ~: [[1, 2], [3, 4]] ~=? parse "1\t2\n3\t4\n"
 testDiff = "testDiff" ~: 6 ~=? diff [2, 7, 8, 3, 6, 4]
 testDiffAll = "testDiffAll" ~: [5, 3] ~=? diffAll [[2, 7, 5], [3, 6, 4]]
+testChecksum = "testChecksum" ~: 8 ~=? checksum [[2, 7, 5], [3, 6, 4]]
 
 splitOnTabs = T.splitOn (pack "\t")
 splitAll = (Prelude.map splitOnTabs) . T.lines . pack
@@ -17,6 +18,8 @@ parse = Prelude.map (Prelude.map $ (read :: String -> Int) . unpack) . splitAll
 
 diff xs = Prelude.maximum xs - Prelude.minimum xs
 diffAll = Prelude.map diff
+
+checksum = sum . diffAll
 
 main = do
     runTestTT allTests
