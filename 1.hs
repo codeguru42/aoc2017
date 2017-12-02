94be14cb1bb8ae391024985dfb62f1a73e5b9445
Code Apprentice <codeguru42@gmail.com>
2017-12-01 21:21:37 -0700

    Day 1: Tests refactored out the yang

--- 1.hs
+++ 1.hs
@@ -2,11 +2,13 @@ import Data.Char (digitToInt)
 import Data.Maybe (catMaybes)
 import Test.HUnit
 
-tests = test [test1_1, test1_2, test1_3, test1_4]
-test1_1 = "test1_1" ~: 3 ~=? (part1 [1, 1, 2, 2])
-test1_2 = "test1_2" ~: 4 ~=? (part1 [1, 1, 1, 1])
-test1_3 = "test1_3" ~: 0 ~=? (part1 [1, 2, 3, 4])
-test1_4 = "test1_1" ~: 9 ~=? (part1 [9, 1, 2, 1, 2, 9])
+part1Fixtures = [ ("test1_1", 3, [1, 1, 2, 2])
+                , ("test1_2", 4, [1, 1, 1, 1])
+                , ("test1_3", 0, [1, 2, 3, 4])
+                , ("test1_1", 9, [9, 1, 2, 1, 2, 9])
+                ]
+part1Test (label, expected, input) = label ~: expected ~=? (part1 input)
+part1Tests = test $ map part1Test part1Fixtures
 
 pairs1 :: [Int] -> [(Int, Int)]
 pairs1 xs = zip xs (tail xs ++ [head xs])
@@ -28,7 +30,7 @@ part2 :: [Int] -> Int
 part2 = day1 . pairs2
 
 main = do
-    runTestTT tests
+    runTestTT part1Tests
     input <- readFile "1.txt"
     let digits = map digitToInt input
     print $ part1 digits
