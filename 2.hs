b723fef795747464e249f60120917d44a6ed5e1e
Code Apprentice <codeguru42@gmail.com>
2017-12-01 23:23:39 -0700

    Day 2: Rename `diff` to `part1`

--- 2.hs
+++ 2.hs
@@ -1,24 +1,22 @@
 import Test.HUnit
 import Data.Text as T
 
-allTests = test [testSplitOnTabs, testSplitAll, testParse, testDiff, testDiffAll, testChecksum]
+allTests = test [testSplitOnTabs, testSplitAll, testParse, testPart1, testChecksum]
 testSplitOnTabs = "testSplitOnTabs" ~: [pack "foo", pack "bar"] ~=? (splitOnTabs $ pack "foo\tbar")
 testSplitAll = "testSplitAll" ~: [ [pack "foo", pack "bar"]
                                    , [pack "fooze", pack "baz"]
                                  ]
                               ~=? splitAll "foo\tbar\nfooze\tbaz\n"
 testParse = "testParse" ~: [[1, 2], [3, 4]] ~=? parse "1\t2\n3\t4\n"
-testDiff = "testDiff" ~: 6 ~=? diff [2, 7, 8, 3, 6, 4]
-testDiffAll = "testDiffAll" ~: [5, 3] ~=? diffAll [[2, 7, 5], [3, 6, 4]]
+testPart1 = "testPart1" ~: 6 ~=? part1 [2, 7, 8, 3, 6, 4]
 testChecksum = "testChecksum" ~: 8 ~=? checksum "2\t7\t 5\n3\t6\t4\n"
 
 splitOnTabs = T.splitOn (pack "\t")
 splitAll = (Prelude.map splitOnTabs) . T.lines . pack
 parse = Prelude.map (Prelude.map $ (read :: String -> Int) . unpack) . splitAll
 
-diff xs = Prelude.maximum xs - Prelude.minimum xs
-diffAll = Prelude.map diff
-checksum = sum . diffAll . parse
+part1 xs = Prelude.maximum xs - Prelude.minimum xs
+checksum = sum . (Prelude.map part1) . parse
 
 main = do
     runTestTT allTests
