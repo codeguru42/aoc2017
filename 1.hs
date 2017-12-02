a746b3ffa2269153e380c30b879930f47caad4cd
Code Apprentice <codeguru42@gmail.com>
2017-12-01 21:41:26 -0700

    Day 1: Add tests for Part 2

--- 1.hs
+++ 1.hs
@@ -2,6 +2,8 @@ import Data.Char (digitToInt)
 import Data.Maybe (catMaybes)
 import Test.HUnit
 
+allTests = test [part1Tests, part2Tests]
+
 part1Fixtures = [ ("test1_1", 3, "1122")
                 , ("test1_2", 4, "1111")
                 , ("test1_3", 0, "1234")
@@ -9,6 +11,15 @@ part1Fixtures = [ ("test1_1", 3, "1122")
                 ]
 part1Test (label, expected, input) = label ~: expected ~=? (part1 input)
 part1Tests = test $ map part1Test part1Fixtures
+
+part2Fixtures = [ ("test2_1", 6, "1212")
+                , ("test2_2", 0, "1221")
+                , ("test2_3", 4, "123425")
+                , ("test2_4", 12, "123123")
+                , ("test2_5", 4, "12131415")
+                ]
+part2Test (label, expected, input) = label ~: expected ~=? (part2 input)
+part2Tests = test $ map part2Test part2Fixtures
 
 pairs1 xs = zip xs (tail xs ++ [head xs])
 pairs2 xs = zip xs (drop n xs ++ take n xs)
@@ -21,7 +32,7 @@ part2 = day1 pairs2
 parse = map digitToInt
 
 main = do
-    runTestTT part1Tests
+    runTestTT allTests
     input <- readFile "1.txt"
     print $ part1 input
     print $ part2 input
