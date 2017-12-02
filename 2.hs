cddfe22e3cef2ef3551596096176f1f054711bbb
Code Apprentice <codeguru42@gmail.com>
2017-12-01 22:46:46 -0700

    Day 2: Second helper function

--- 2.hs
+++ 2.hs
@@ -1,10 +1,18 @@
 import Test.HUnit
 import Data.Text as T
 
-allTests = test [testSplitOnTabs]
+allTests = test [testSplitOnTabs, testParse]
 testSplitOnTabs = "testSplitOnTabs" ~: [pack "foo", pack "bar"] ~=? (splitOnTabs $ pack "foo\tbar")
+testParse = "testParse" ~: [ [pack "foo", pack "bar"]
+                           , [pack "fooze", pack "baz"]
+                           ]
+                        ~=? parse "foo\tbar\nfooze\tbaz\n"
 
 splitOnTabs = T.splitOn (pack "\t")
 
+parse = (Prelude.map splitOnTabs) . T.lines . pack
+
 main = do
     runTestTT allTests
+    -- input <- readFile "1.txt"
+    --print $ parse input
