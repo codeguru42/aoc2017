0b7451d3833e17a4442f0d4dac70b914a02693cd
Code Apprentice <codeguru42@gmail.com>
2017-12-02 22:27:42 -0700

    Day 3: Add tests

--- 3.hs
+++ 3.hs
@@ -1,4 +1,13 @@
+import Test.HUnit
+
+testCases :: [(Int, Int)]
+testCases = [(1, 0), (12, 3), (23, 2), (1024, 31)]
+makeTest (input, expected) = expected ~=? steps input
+tests = test $ map makeTest testCases
+
+steps :: Int -> Int
 steps = undefined
 
 main = do
+    runTestTT tests
     print $ steps 368078
