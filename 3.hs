de143d16e72b223c5da9a5ae2a73f37d5ac8de16
Code Apprentice <codeguru42@gmail.com>
2017-12-03 00:46:23 -0700

    Day 3: Modify implementation of `coords` and add more tests

--- 3.hs
+++ 3.hs
@@ -1,3 +1,5 @@
+module Main where
+
 import Test.HUnit
 
 allTests = test [testSteps, testCoords, testSumNeighbors,testNeighbors]
@@ -7,7 +9,7 @@ makeTest f (input, expected) = expected ~=? f input
 mapTests f = test . map (makeTest f)
 testSteps = mapTests steps stepsTestCases
 
-coordsTestCases = [(1, (0, 0)), (2, (1, 0)), (5, (-1, 1)), (12, (2, 1))]
+coordsTestCases = [(1, (0, 0)), (2, (1, 0)), (5, (-1, 1)), (12, (2, 1)), (20, (-2, -1)), (23, (0, -2)), (35, (-1, 3))]
 testCoords = mapTests coords coordsTestCases
 
 neighborsTestCases = [(1, []), (2, [1]), (3, [1, 2]), (4, [1, 2, 3]), (5, [1, 4]), (6, [1, 4, 5]), (7, [1, 6]), (8, [1, 6, 7])]
@@ -18,25 +20,21 @@ testSumNeighbors = mapTests sumNeighbors sumNeighborsTestCases
 
 coords :: Int -> (Int, Int)
 coords 1 = (0, 0)
-coords 2 = (1, 0)
-coords 3 = (1, 1)
-coords 4 = (0, 1)
 coords n
-    | n == sq = let (x, y) = coords ((evrt - 2) * (evrt - 2))
-                in (x - 1, y + 1)
+    | n == sq = let (x, y) = coords ((oddrt - 2) * (oddrt - 2))
+                in (x + 1, y - 1)
     | otherwise = let (x, y) = coords sq
                   in case group of
-                    0 -> (x - 1, y - index)
-                    1 -> (x + index, y - evrt)
-                    2 -> (x + evrt, y - (evrt - 1) + index)
-                    3 -> (x + (evrt - 1) - index, y + 1)
+                    0 -> (x + 1, y + index)
+                    1 -> (x - index, y + oddrt)
+                    2 -> (x - oddrt, y + (oddrt - 1) - index)
+                    3 -> (x - (oddrt - 1) + index, y - 1)
     where rt = floor . sqrt $ fromIntegral n
-          evrt = if rt `mod` 2 == 0 then rt else rt - 1
-          sq = evrt * evrt
-          (x, y) = coords sq
+          oddrt = if rt `mod` 2 == 0 then rt - 1 else rt
+          sq = oddrt * oddrt
           diff = n - sq - 1
-          group = diff `div` (evrt + 1)
-          index = diff `mod` (evrt + 1)
+          group = diff `div` (oddrt + 1)
+          index = diff `mod` (oddrt + 1)
 
 steps :: Int -> Int
 steps n = abs x + abs y
