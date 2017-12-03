39f55c0272b683e2df51392432210ef83f000099
Code Apprentice <codeguru42@gmail.com>
2017-12-02 22:47:18 -0700

    Day 3: Implement `steps` in terms of a helper function `coords`

--- 3.hs
+++ 3.hs
@@ -5,8 +5,12 @@ testCases = [(1, 0), (12, 3), (23, 2), (1024, 31)]
 makeTest (input, expected) = expected ~=? steps input
 tests = test $ map makeTest testCases
 
+coords :: Int -> (Int, Int)
+coords = undefined
+
 steps :: Int -> Int
-steps = undefined
+steps n = abs x + abs y
+    where (x, y) = coords n
 
 main = do
     runTestTT tests
