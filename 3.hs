bed8e1a9e24147102d3bbb913c5471ad93f8f9fb
Code Apprentice <codeguru42@gmail.com>
2017-12-02 22:53:09 -0700

    Day 3: Write tests for `coords`

--- 3.hs
+++ 3.hs
@@ -1,9 +1,13 @@
 import Test.HUnit
 
-testCases :: [(Int, Int)]
-testCases = [(1, 0), (12, 3), (23, 2), (1024, 31)]
-makeTest (input, expected) = expected ~=? steps input
-tests = test $ map makeTest testCases
+allTests = test [testSteps, testCoords]
+
+stepsTestCases = [(1, 0), (12, 3), (23, 2), (1024, 31)]
+makeTest f (input, expected) = expected ~=? f input
+testSteps = test $ map (makeTest steps) stepsTestCases
+
+coordsTestCases = [(1, (0, 0)), (2, (1, 0)), (5, (-1, 1)), (12, (2, 1))]
+testCoords = test $ map (makeTest coords) coordsTestCases
 
 coords :: Int -> (Int, Int)
 coords = undefined
@@ -13,5 +17,5 @@ steps n = abs x + abs y
     where (x, y) = coords n
 
 main = do
-    runTestTT tests
+    runTestTT allTests
     print $ steps 368078
