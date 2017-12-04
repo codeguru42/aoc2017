7f4ae468d885a2dc6e33cb35e4573bdd9950e4a9
Code Apprentice <codeguru42@gmail.com>
2017-12-03 22:35:25 -0700

    Day 4: Test if two words are anagrams

--- 4.hs
+++ 4.hs
@@ -1,14 +1,19 @@
 import Test.HUnit
+import Data.List (sort)
 import Data.Text as T
 
-tests = test [testValidPassPhrase, testInvalidPassPhrase]
+tests = test [testValidPassPhrase, testInvalidPassPhrase, testIsAnagram, testIsNotAnagram]
 testValidPassPhrase = True ~=? isValidPassPhrase "aa bb cc dd ee"
 testInvalidPassPhrase = False ~=? isValidPassPhrase "aa bb cc dd aa"
+testIsAnagram = True ~=? isAnagramOf "abcde" "ecdab"
+testIsNotAnagram = False ~=? isAnagramOf "abcde" "fghij"
 
 isValidPassPhrase s = noDupes xs
     where xs = T.splitOn (pack " ") (pack s)
           noDupes [] = True
           noDupes (y:ys) = (not $ y `elem` ys) && (noDupes ys)
+
+isAnagramOf x y = (sort x) == (sort y)
 
 main = do
     runTestTT tests
