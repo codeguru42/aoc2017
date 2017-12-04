d8b8cb870ca2331687b42f10798c0c72fcacbc36
Code Apprentice <codeguru42@gmail.com>
2017-12-03 23:02:05 -0700

    Day 4: Final soltuion Part 2

--- 4.hs
+++ 4.hs
@@ -1,11 +1,9 @@
 import Test.HUnit
 import Data.List (sort)
 
-tests = test [testValidPassPhrase1, testInvalidPassPhrase1, testIsAnagram, testIsNotAnagram]
+tests = test [testValidPassPhrase1, testInvalidPassPhrase1]
 testValidPassPhrase1 = True ~=? isValidPassPhrase1 "aa bb cc dd ee"
 testInvalidPassPhrase1 = False ~=? isValidPassPhrase1 "aa bb cc dd aa"
-testIsAnagram = True ~=? isAnagramOf "abcde" "ecdab"
-testIsNotAnagram = False ~=? isAnagramOf "abcde" "fghij"
 
 validate1 [] = True
 validate1 (y:ys) = (not $ y `elem` ys) && (validate1 ys)
@@ -14,9 +12,12 @@ isValidPassPhrase1 = isValidPassPhrase validate1
 isValidPassPhrase validate s = validate $ words s
 countValidPassPhrases f = length . filter f
 
-isAnagramOf x y = (sort x) == (sort y)
+validate2 xs = validate1 xs'
+    where xs' = map sort xs
+isValidPassPhrase2 = isValidPassPhrase validate2
 
 main = do
     runTestTT tests
     input <- readFile "4.txt"
     print . countValidPassPhrases isValidPassPhrase1 $ lines input
+    print . countValidPassPhrases isValidPassPhrase2 $ lines input
