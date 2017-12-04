64032599750aec615110ace433a10f8a6de3581e
Code Apprentice <codeguru42@gmail.com>
2017-12-03 22:30:54 -0700

    Day 4: Final solution for Part 1

--- 4.hs
+++ 4.hs
@@ -5,8 +5,12 @@ tests = test [testValidPassPhrase, testInvalidPassPhrase]
 testValidPassPhrase = True ~=? isValidPassPhrase "aa bb cc dd ee"
 testInvalidPassPhrase = False ~=? isValidPassPhrase "aa bb cc dd aa"
 
-isValidPassPhrase s = not $ x `elem` xs
-    where (x:xs) = T.splitOn (pack " ") (pack s)
+isValidPassPhrase s = noDupes xs
+    where xs = T.splitOn (pack " ") (pack s)
+          noDupes [] = True
+          noDupes (y:ys) = (not $ y `elem` ys) && (noDupes ys)
 
 main = do
     runTestTT tests
+    input <- readFile "4.txt"
+    print . Prelude.length . Prelude.filter isValidPassPhrase $ Prelude.lines input
