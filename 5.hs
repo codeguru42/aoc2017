9085a3e823d5e95e408fc726cbe68efad2392572
Code Apprentice <codeguru42@gmail.com>
2017-12-04 23:13:05 -0700

    Day 5: More tests

--- 5.hs
+++ 5.hs
@@ -2,6 +2,10 @@ import Test.HUnit
 
 tests = test [testNewInstructions]
 newInstructionsFixtures = [ ([1, 3, 0, 1, -3], 0, [0, 3, 0, 1, -3])
+                          , ([2, 3, 0, 1, -3], 0, [1, 3, 0, 1, -3])
+                          , ([2, 4, 0, 1, -3], 1, [2, 3, 0, 1, -3])
+                          , ([2, 4, 0, 1, -2], 4, [2, 4, 0, 1, -3])
+                          , ([2, 5, 0, 1, -2], 1, [2, 4, 0, 1, -2])
                           ]
 makeNewInstructionsTest (expected, i, instructions) = expected ~=? newInstructions i instructions
 testNewInstructions = test $ map makeNewInstructionsTest newInstructionsFixtures
