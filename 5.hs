e02fd9d0299c62c53f11a199b0e051650201e928
Code Apprentice <codeguru42@gmail.com>
2017-12-04 23:08:16 -0700

    Day 5: Refactor tests

--- 5.hs
+++ 5.hs
@@ -1,7 +1,10 @@
 import Test.HUnit
 
 tests = test [testNewInstructions]
-testNewInstructions = [1, 3, 0, 1, -3] ~=? newInstructions 0 [0, 3, 0, 1, -3]
+newInstructionsFixtures = [ ([1, 3, 0, 1, -3], 0, [0, 3, 0, 1, -3])
+                          ]
+makeNewInstructionsTest (expected, i, instructions) = expected ~=? newInstructions i instructions
+testNewInstructions = test $ map makeNewInstructionsTest newInstructionsFixtures
 
 newInstructions i instructions = take i instructions ++ [j + 1] ++ drop (i + 1) instructions
     where j = instructions !! i
