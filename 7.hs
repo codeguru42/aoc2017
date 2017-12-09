14d830d3ecabeb516dc1ed62323adb88ae6e18fb
Code Apprentice <codeguru42@gmail.com>
2017-12-08 19:02:55 -0700

    Day 7: Parse input

--- 7.hs
+++ 7.hs
@@ -1,3 +1,12 @@
+parseLine l = (name, weight, children)
+    where (name:weight':rest) = words l
+          weight = init $ tail weight'
+          children | rest == [] = []
+                   | otherwise = [init child' | child' <- children']
+                        where (_:children') = rest
+
+parseAll = map parseLine . lines
+
 main = do
     input <- readFile "7.txt"
-    print $ lines input
+    print $ parseAll input
