50291ef97f65161ebfce9ea6c18bfda10bd5877e
Code Apprentice <codeguru42@gmail.com>
2017-12-10 10:01:07 -0700

    Day 6: Read and parse input file

--- 6.py
+++ 6.py
@@ -1,5 +1,11 @@
 def main():
-    pass
+    file = open('6.txt')
+    banks = []
+    for line in file:
+        cells = [int(i) for i in line.split()]
+        banks.extend(cells)
+    print(banks)
+
 
 if __name__ == '__main__':
     main()
