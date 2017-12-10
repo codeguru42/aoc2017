d28cb792014f89b9edd792339cba14303e05b3e3
Code Apprentice <codeguru42@gmail.com>
2017-12-10 09:17:02 -0700

    Day 5: Refactor Part 1 solution to its own funciton

--- 5.py
+++ 5.py
@@ -1,16 +1,17 @@
-def main():
-    file = open('5.txt')
-    jumps = [int(line) for line in file]
-
+def part1(jumps):
     count = 0
     address = 0
     while 0 <= address < len(jumps):
-        print(address, count)
         new_address = address + jumps[address]
         jumps[address] += 1
         address = new_address
         count += 1
-    print(count)
+    return count
+
+def main():
+    file = open('5.txt')
+    jumps = [int(line) for line in file]
+    print(part1(jumps))
 
 if __name__ == '__main__':
     main()
