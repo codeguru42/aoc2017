8470c48b3c07eb2f5bafe6edb91813183bd6ba11
Code Apprentice <codeguru42@gmail.com>
2017-12-10 09:20:42 -0700

    Day 5: Refactor more for reuse for Part 2

--- 5.py
+++ 5.py
@@ -1,17 +1,24 @@
-def part1(jumps):
+def do_jumps(jumps, f):
     count = 0
     address = 0
     while 0 <= address < len(jumps):
         new_address = address + jumps[address]
-        jumps[address] += 1
+        f(jumps, address)
         address = new_address
         count += 1
     return count
 
+
+def part1(jumps):
+    def f(jumps, address):
+        jumps[address] += 1
+    return do_jumps(jumps, f)
+
+
 def main():
     file = open('5.txt')
     jumps = [int(line) for line in file]
-    print(part1(jumps))
+    print(part1(jumps.copy()))
 
 if __name__ == '__main__':
     main()
