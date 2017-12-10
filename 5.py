87ba7f2da107f6b4c4ce502c9ff5fcaca7e033cd
Code Apprentice <codeguru42@gmail.com>
2017-12-10 09:15:05 -0700

    Day 5: Solution for Part 1

--- 5.py
+++ 5.py
@@ -1,7 +1,16 @@
 def main():
     file = open('5.txt')
     jumps = [int(line) for line in file]
-    print(jumps)
+
+    count = 0
+    address = 0
+    while 0 <= address < len(jumps):
+        print(address, count)
+        new_address = address + jumps[address]
+        jumps[address] += 1
+        address = new_address
+        count += 1
+    print(count)
 
 if __name__ == '__main__':
     main()
