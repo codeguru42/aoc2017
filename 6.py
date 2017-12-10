6e76c4996ad807bfdb191174ed38637934175772
Code Apprentice <codeguru42@gmail.com>
2017-12-10 10:16:30 -0700

    Day 6: Final solution for Part 1

--- 6.py
+++ 6.py
@@ -4,7 +4,17 @@ def main():
     for line in file:
         cells = [int(i) for i in line.split()]
         banks.extend(cells)
-    print(banks)
+
+    count = 0
+    all_banks = []
+    while banks not in all_banks:
+        all_banks.append(banks.copy())
+        i, m = max(enumerate(banks), key=lambda x: x[1])
+        banks[i] = 0
+        for x in range(m):
+            banks[(i + x + 1) % len(banks)] += 1
+        count += 1
+    print(count)
 
 
 if __name__ == '__main__':
