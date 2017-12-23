237e9bea0afc0e5028d033b8e1d442076aa4d9ff
Code Apprentice <codeguru42@gmail.com>
2017-12-22 22:39:37 -0700

    Day 23: Parse input

--- 23.py
+++ 23.py
@@ -1,7 +1,14 @@
-def main():
+def parse():
     file = open('23.txt')
+    instructions = []
     for line in file:
-        print(line)
+        instructions.append(line.split())
+    return instructions
+
+
+def main():
+    instructions = parse()
+    print(instructions)
 
 
 if __name__ == '__main__':
