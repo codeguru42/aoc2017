36271e02908d3f99ded134bd03bb9f7d5233f4c3
Code Apprentice <codeguru42@gmail.com>
2017-12-22 23:36:09 -0700

    Day 22: Parse input

--- 22.py
+++ 22.py
@@ -1,5 +1,14 @@
+def parse():
+    file = open('22.txt')
+    mem = []
+    for line in file:
+        mem.append(list(line.strip()))
+    return mem
+
+
 def main():
-    pass
+    mem = parse()
+    print(mem)
 
 
 if __name__ == '__main__':
